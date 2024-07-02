239e44ddc176c5b188451636e0f0606e665cab48
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2024-07-02 16:09:37 +0000

    hot fixing

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path  
-from .views import Tasks, TaskDetail  
+from .views import Tasks, TaskDetail, hello_world 
   
 urlpatterns = [
     path("", Tasks.as_view()),   
     path("<str:pk>", TaskDetail.as_view()),
+    path("hello", hello_world, name="hello")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 from django.shortcuts import render  
 from rest_framework.response import Response  
-from rest_framework import status, generics  
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
 from .models import TaskModel  
 from .serializers import TaskSerializer  
 import math  
@@ -102,3 +103,8 @@ class TaskDetail(generics.GenericAPIView):
         task.delete()  
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+
+@api_view()
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
