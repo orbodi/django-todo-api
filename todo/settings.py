d5499373dbbf8a0ffc62b9c3c41b962194b1f74c
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2024-07-02 16:21:41 +0000

    hot fixing

--- todo/settings.py
+++ todo/settings.py
@@ -25,10 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%--p6d$*lw0rv-#_*=@!hgx)7hb8nt$95p0vhn7t$-8@23l@n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+DEBUG = 'RENDER' not in os.environ
 
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
