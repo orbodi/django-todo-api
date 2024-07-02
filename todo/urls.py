7e674034ca2a3d1792955a7dcc1057808d64b104
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2024-07-02 15:42:14 +0000

    adding API documentation Swagger/Redoc

--- todo/urls.py
+++ todo/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
   
 urlpatterns = [  
     path("admin/", admin.site.urls),  
-    path("api/tasks/", include("todo.urls")),  
+    path("api/tasks/", include("tasks.urls")),  
     re_path(  
         r"^swagger(?P<format>\.json|\.yaml)$",  
         schema_view.without_ui(cache_timeout=0),  
